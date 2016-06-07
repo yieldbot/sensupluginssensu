aac79327a7cbde4e134c4d4f0c6434ed60f4c85f
Jenkins <Jenkins@yieldbot.com>
2016-06-07 15:51:50 +0000

    [sensupluginssensu] Development Version Bump

--- version/version.go
+++ version/version.go
@@ -31,7 +31,7 @@ const AppVersionMajor = "0"
 const AppVersionMinor = "0"
 
 // AppVersionPatch is the patch version
-const AppVersionPatch = "2"
+const AppVersionPatch = "3"
 
 // AppVersionPre ...
 const AppVersionPre = ""
