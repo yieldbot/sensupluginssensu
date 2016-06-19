656c08ce302372b46f329b7e31fa42794163c8f7
Matt Jones <mattjones@yieldbot.com>
2016-06-18 22:06:02 -0400

    working on struct for sensu config

--- sensupluginssensu/common.go
+++ sensupluginssensu/common.go
@@ -29,10 +29,39 @@ import (
 	"github.com/yieldbot/sensupluginssensu/version"
 )
 
+type SensuConfig struct {
+	Sensu struct {
+		Spawn struct {
+			Limit int
+		}
+		Environment string
+		FQDN        string
+		Hostname    string
+	}
+	Transport struct {
+		Name string
+	}
+	Check
+	Client
+}
+
+type Check struct {
+	Type     string
+	Playbook string
+}
+
+type Client struct {
+	Address string
+	Name    string
+}
+
 // AcquireLocalChecks will retrieve the currently running configuration and
 // return a list of all checks it knows about
 func AcquireLocalChecks() {
-	var jsonOut map[string][]string
+
+	jsonOut := new(SensuConfig)
+	// config = sensuEvent.AcquireSensuEvent()
+	// var jsonOut map[string]interface{}
 	localChecks := exec.Command("/opt/sensu/embedded/bin/sensu-client", "-L", "error", "-d", "/etc/sensu/conf.d", "-P")
 
 	out, err := localChecks.Output()
@@ -44,6 +73,7 @@ func AcquireLocalChecks() {
 			"error":   err,
 		}).Error(`Local Checks returned invalid output`)
 	}
+	// fmt.Println(string(out))
 
 	err = json.Unmarshal(out, &jsonOut)
 	if err != nil {
@@ -55,7 +85,19 @@ func AcquireLocalChecks() {
 			"output":  out,
 		}).Error(`Could not unmarshall the json.`)
 	}
-
 	fmt.Println(jsonOut)
-	fmt.Println(jsonOut["transport"])
+	fmt.Println(jsonOut.Sensu)
+	fmt.Println(jsonOut.Sensu.Spawn)
+	fmt.Println(jsonOut.Sensu.Spawn.Limit)
+	// fmt.Println(jsonOut["transport"])
+	// checks := jsonOut["checks"]
+	// fmt.Println(checks)
+	// // fmt.Println(jsonOut["transport"]["name"])
+	// args.([]interface{})[0].(map[string]interface{})["time"].(string)
+
 }
+
+// "transport":{
+//     "name":"rabbitmq",
+//     "reconnect_on_error":true
+//   },
